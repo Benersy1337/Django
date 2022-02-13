9d71ff6a613e79ca39472b3dd23710930da66dc1
Luan Roberto <benersy1337@gmail.com>
2022-02-13 19:01:54 -0300

    modelos registrados no painel admin, fixture para testes

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from catalogo.models import Genero, Linguagem, Autor, Livro, ExemplarLivro
+
+
+# Registro para poder usar no painel admin do django
+admin.site.register(Genero)
+admin.site.register(Linguagem)
+admin.site.register(Autor)
+admin.site.register(Livro)
+admin.site.register(ExemplarLivro)
+
+
